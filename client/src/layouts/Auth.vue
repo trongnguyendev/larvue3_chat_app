45b726ba5814a4ddd35af8262701fd888b80fb4c
trongnguyendev <trongnguyendev@gmail.com>
2023-02-13 00:29:18 +0700

    test test

--- client/src/layouts/Auth.vue
+++ client/src/layouts/Auth.vue
@@ -2,6 +2,7 @@
     <div class="flex justify-center items-center w-full bg-gray-500 auth_bg">
         <div class="auth w-96 rounded-md overflow-hidden">
             <router-view></router-view>
+            .
         </div>
     </div>
 </template>
